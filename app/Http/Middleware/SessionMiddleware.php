0befb16886f37749bc2e549a6f05f6836c2574e7
Javier Ojeda <hola@javierojeda.co>
2016-04-03 19:15:27 -0500

    remove token verification when signup and login

--- app/Http/Middleware/SessionMiddleware.php
+++ app/Http/Middleware/SessionMiddleware.php
@@ -18,21 +18,18 @@ class SessionMiddleware
     public function handle($request, Closure $next)
     {
         $path = $request->path();
-        if($path === 'api/v1/signup'){
+        if($path === 'api/v1/signup' || $path === 'api/v1/login'){
             return $next($request);
         }
-        $email = $request->email;
-        $password = $request->password;
         $auth_token = $request->auth_token;
-        $user = User::where('email',$email)->where('password',md5($password))->first();
+        $user = User::where('auth_token',$auth_token)->first();
         if($user !== null && $auth_token === $user->auth_token){
             return $next($request);
         }
         else{
-            $error = ($user === null)? 'Wrong credentials!' : "Token error!";
             return response()->json([
                 'error' => [
-                    'message' => $error,
+                    'message' => 'Token error!',
                     'status_code' => '403'
                 ]
             ])->header('Content-Type', 'application/json');;
